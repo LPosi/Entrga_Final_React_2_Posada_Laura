a1af1dd37bf181253a2a9123e411385967b46073
LPosi <lrposada@yahoo.com.ar>
2026-01-27 11:40:42 -0300

    ProyectoFinal+Posada

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
 
 export const CartContext = createContext();
 
@@ -38,3 +38,5 @@ export const CartProvider = ({ children }) => {
     </CartContext.Provider>
   );
 };
+
+export const useCart = () => useContext(CartContext);
